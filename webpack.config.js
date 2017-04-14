0ef5fe211451e427c52d3bbad5352604aef7aa1a
ぬこすけ <39.nuko@gmail.com>
2017-04-14 09:06:49 +0000

    init

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+    entry: {
+        app: "./index.ts",
+    },
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "[name].bundle.js",
+    },
+    resolve: {
+        extensions: ["*", ".ts", ".tsx", ".js", ".jsx"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [{ loader: "ts-loader" }],
+            },
+        ],
+    },
+};
