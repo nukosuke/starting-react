3f5c82841123e805a9862ec183bb8e047c8983da
ぬこすけ <39.nuko@gmail.com>
2017-04-18 11:15:09 +0000

    config reporter

--- tests/HelloWorld.spec.ts
+++ tests/HelloWorld.spec.ts
@@ -1,7 +1,10 @@
 import HelloWorld from "../HelloWorld";
+import * as chai from "chai";
+
+const expect = chai.expect;
 
 describe("HelloWorld React component", () => {
     it("test", () => {
-        
+        expect(1).to.be.a('number');
     });
 });
